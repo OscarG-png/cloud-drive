c7bae0ca43071d1e77446420323403c37b0ceaeb
OscarG <oscar.gamez88@gmail.com>
2025-02-12 13:24:28 -0800

    schemas for files and folders

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,41 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  bigint,
+  text,
+  singlestoreTableCreator,
+  index,
+} from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+  (name) => `drive_tutorial_${name}`,
+);
+
+export const files = createTable(
+  "files_tables",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name"),
+    size: int("size"),
+    url: text("url"),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = createTable(
+  "folders_tables",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
