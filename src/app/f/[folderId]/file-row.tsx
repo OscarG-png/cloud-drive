2c8bc3ee5235e4c3478f4aaf69aa09c29837adc6
OscarG <oscar.gamez88@gmail.com>
2025-02-16 13:41:10 -0800

    removed un-need file sizes

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -10,7 +10,7 @@ function formatFileSize(bytes: number): string {
   }
 
   const k = 1024;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
